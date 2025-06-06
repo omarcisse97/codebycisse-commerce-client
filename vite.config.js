7405cda68926d8c5e8a38b29ece636775180b803
omar cisse <omarcisse97@knights.ucf.edu>
2025-06-06 02:12:08 -0400

    Updated allowed hosts in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    allowedHosts: 'all'
   }
 })
