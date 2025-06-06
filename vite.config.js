6a8516d9e28ba7b1176bf64b20bb5b97ad7a25e9
omar cisse <omarcisse97@knights.ucf.edu>
2025-06-06 01:57:27 -0400

    Updated vite config and package.json for production build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8000,
-  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000
+  }
 })
