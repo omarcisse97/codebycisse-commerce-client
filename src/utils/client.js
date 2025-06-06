a3562c4646c1249d9eacdbcc157d1a271456349b
omar cisse <omarcisse97@knights.ucf.edu>
2025-06-06 02:49:48 -0400

    Updated medusa client to use environment variables

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,7 +2,7 @@ import Medusa from "@medusajs/js-sdk"
 
 const medusaClient = new Medusa({
   baseUrl: import.meta.env.VITE_MEDUSA_SERVER_URL,
-  publishableKey: "pk_9a004279a64215115541ef7e5d961b557596bbd1aeac0028cb6b14a44df9c0a0",
+  publishableKey: import.meta.env.VITE_API_KEY_MEDUSA_SERVER,
 })
 
 export { medusaClient }
